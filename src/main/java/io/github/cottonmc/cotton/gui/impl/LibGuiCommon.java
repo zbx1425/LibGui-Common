ad66f63cf47554f71ce56b0cd76e9a34e0cfab00
zbx1425 <support@zbx1425.cn>
2026-04-03 22:12:19 +0800

    change: revert modid idea

--- src/main/java/io/github/cottonmc/cotton/gui/impl/LibGuiCommon.java
+++ src/main/java/io/github/cottonmc/cotton/gui/impl/LibGuiCommon.java
@@ -3,7 +3,7 @@ package io.github.cottonmc.cotton.gui.impl;
 import net.minecraft.resources.Identifier;
 
 public final class LibGuiCommon {
-	public static final String MOD_ID = "libgui_common";
+	public static final String MOD_ID = "libgui";
 
 	public static Identifier id(String path) {
 		return Identifier.fromNamespaceAndPath(MOD_ID, path);
